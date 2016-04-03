5b817ed61afcfb0171c6561a50415bbc6702e034
pedroescudero <pedroescudero.zumel@gmail.com>
2016-04-03 22:54:02 +0200

    Added class_eval to module

--- lib/mongoid-friendly-url.rb
+++ lib/mongoid-friendly-url.rb
@@ -1,24 +1,23 @@
-require 'active_support/concern'
-module Concerns::Sluged
-  extend ActiveSupport::Concern
-  
-  included do
+module Sluged
 
-  		field :slug, type: String
+	def self.included(base)
+		base.class_eval do
+		  scope :named, -> { where(name: "Pedro") }
+		  field :slug, type: String
+		  before_save :make_slug
+		end
+	end
 
-  		before_save :make_slug
+	def to_param
+		slug
+	end
 
-	  	def to_param
-	  		slug
-	  	end
+	def make_slug		
+		self.slug = send(self.class::FIELD_FOR_SLUG).gsub(" ","-")
+	end
 
-	  	def make_slug		
-			self.slug = send(self.class::FIELD_FOR_SLUG).gsub(" ","-")
-	  	end
+	def self.find(id)
+		self.where(slug:id).present? ? self.where(slug:id).first : super
+	end	
 
-	  	def self.find(id)
-	  		self.where(slug:id).present? ? self.where(slug:id).first : super
-  		end	
-
-  	end
-end	
+end
